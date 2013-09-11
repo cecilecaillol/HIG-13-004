d6f5ca241957e0bfc38cdd7abcc372b73ca81cf4
cecilecaillol <cecilecaillol@gmail.com>
2013-09-11 10:49:27 +0200

    to sort plots directory

---
+++ organize_plot_directory.sh
@@ -0,0 +1,55 @@
+ cd plots
+ mkdir ZH
+ mkdir LLT
+ mkdir LTT
+ mkdir CMB
+ mkdir ZH/prefit
+ mkdir ZH/postfit
+ mkdir ZH/limit
+ mkdir LTT/limit
+ mkdir LLT/postfit
+ mkdir LTT/postfit
+ mkdir LTT/prefit
+ mkdir LLT/prefit
+ mv llet*prefit.pdf ZH/prefit/.
+ mv llet*prefit*.pdf ZH/prefit/.
+ mv llem*prefit*.pdf ZH/prefit/.
+ mv llmt*prefit*.pdf ZH/prefit/.
+ mv lltt*prefit*.pdf ZH/prefit/.
+ mv lltt*postfit*.pdf ZH/postfit/.
+ mv llet*postfit*.pdf ZH/postfit/.
+ mv llem*postfit*.pdf ZH/postfit/.
+ mv llmt*postfit*.pdf ZH/postfit/.
+ mv emt*postfit*.pdf LLT/postfit/.
+ mv eet*postfit*.pdf LLT/postfit/.
+ mv mmt*postfit*.pdf LLT/postfit/.
+ mv mmt*prefit*.pdf LLT/prefit/.
+ mv emt*prefit*.pdf LLT/prefit/.
+ mv eet*prefit*.pdf LLT/prefit/.
+ mv ett*prefit*.pdf LTT/prefit/.
+ mv mtt*prefit*.pdf LTT/prefit/.
+ mv mtt*postfit*.pdf LTT/postfit/.
+ mv ett*postfit*.pdf LTT/postfit/.
+ mv ltt*postfit*.pdf LTT/postfit/.
+ mv ltt*prefit*.pdf LTT/prefit/.
+ mv llt*prefit*.pdf LLT/prefit/.
+ mv llt*postfit*.pdf LLT/postfit/.
+ mv zh*postfit*.pdf ZH/postfit/.
+ mv zh*prefit*.pdf ZH/prefit/.
+ mv *zh*limit*.* ZH/limit/.
+ mv llem*limit*.* ZH/limit/.
+ mv llet*limit*.* ZH/limit/.
+ mv llmt*limit*.* ZH/limit/.
+ mv lltt*limit*.* ZH/limit/.
+ mv ett*limit*.* LTT/limit/.
+ mv mtt*limit*.* LTT/limit/.
+ mv *wh_had*limit*.* LTT/limit/.
+ mv *wh_*limit*.* LLT/limit/.
+ mv emt*limit*.* LLT/limit/.
+ mv eet*limit*.* LLT/limit/.
+ mv mmt*limit*.* LLT/limit/.
+ mv *zh*significance*.* ZH/limit/.
+ mv *wh_had*significance*.* LTT/limit/.
+ mv *wh_*significance*.* LLT/limit/.
+ mv cmb* CMB/.
+ mv *breakdown* CMB/.
